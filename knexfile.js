eb326f5b7e5a0ede6ce4b234d8124ac6eafa8231
Annique Nsabimana <anniquensabimana@gmail.com>
2019-09-26 13:06:16 -0500

    fixing ends points in heroku

--- knexfile.js
+++ knexfile.js
@@ -1,58 +1,50 @@
-// Update with your 
-
-
-// const localPg = {
-//   host: 'localhost',
-//   database: 'items',
-//   user: 'Admin',
-//   password: 'Testing1'
-// }
-const productionDbConnection = process.env.DATABASE_URL 
+// Update with your config settings.
 
 module.exports = {
-
   development: {
     client: 'sqlite3',
     useNullAsDefault: true,
     connection: {
-      filename: './database/auth.db3'
+      filename: './data/sqlite3.db3',
+    },
+    pool: {
+      afterCreate: (conn, done) => {
+        conn.run('PRAGMA foreign_keys = ON', done);
+      },
     },
     migrations: {
-      directory: './database/migrations'
+      directory: './data/migrations',
     },
     seeds: {
-      directory: './database/seeds'
-    }
+      directory: './data/seeds',
+    },
   },
-
   testing: {
     client: 'sqlite3',
-      connection: {
-        filename: './database/test.db3',
-      },
-      useNullAsDefault: true,
-      migrations: {
-        directory: './database/migrations',
-      },
-      seeds: {
-        directory: './database/seeds',
-      },
-    },
-    
-  
-  production: {
-    client: 'pg',
-    connection: productionDbConnection,
-    pool: {
-      min: 2,
-      max: 10
+    useNullAsDefault: true,
+    connection: {
+      filename: './data/test.db3',
     },
     migrations: {
-      directory: './database/migrations',
+      directory: './data/migrations',
     },
     seeds: {
-      directory: './database/seeds',
+      directory: './data/seeds',
     },
-  }
-
+  },
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+    pool: {
+      afterCreate: (conn, done) => {
+        conn.run('PRAGMA foreign_keys = ON', done);
+      },
+    },
+    migrations: {
+      directory: './data/migrations',
+    },
+    seeds: {
+      directory: './data/seeds',
+    },
+  },
 };
